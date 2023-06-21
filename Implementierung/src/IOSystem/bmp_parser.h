8ed643d3fd5befc60599e1646d727df35a3c6eba
Nico <nico.lintner@tum.de>
2023-06-21 17:07:44 +0200

    some refactoring, split up into multiple files

--- Implementierung/src/IOSystem/bmp_parser.h
+++ Implementierung/src/IOSystem/bmp_parser.h
@@ -1,18 +1,4 @@
-#include <stddef.h>
-#include <stdint.h>
+#include "bmpDefinitions.h"
 
-typedef struct pixel24_t {
-    uint8_t b;
-    uint8_t g;
-    uint8_t r;
-} pixel24_t;
-
-typedef struct uBMPImage {
-    uint32_t pxWidth;
-    uint32_t pxHeight;
-    pixel24_t* pxArray;
-} uBMPImage;
-
-void* readBMPFile (const char* path, size_t* bufSize);
-
-int parseBMPFile (const void* buf, size_t bufSize, uBMPImage* bmpImgBuf);
+int bmpToArray (const void* buf, size_t bufSize, uBMPImage* bmpImgBuf);
+void* arrayToBmp (const uBMPImage* bmpImgBuf, size_t* bufSize);
