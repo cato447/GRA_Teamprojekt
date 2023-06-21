3cf10cfd1307817d39ef04e7547fb16320b95910
Simon Bußmann <simon.bussmann@tum.de>
2023-06-21 17:38:32 +0200

    Tests for colorOfPixel and setPixelAdd + fixed bug in both methods
    - colorOfPixel and setPixelAdd ignored that a pixel is 24 bit wide and treated it as 8 bit wide
    - parseBMPFile returns 1 on fail now

--- Implementierung/src/IOSystem/bmp_parser.c
+++ Implementierung/src/IOSystem/bmp_parser.c
@@ -53,30 +53,30 @@ void* readBMPFile(const char* path, size_t* bufSize) {
 #define PXWIDTH_OFFS 0x12
 #define PXHEIGHT_OFFS 0x16
 /*
-Returns 0 on success and -1 on failure.
+Returns 0 on success and 1 on failure.
 Sets "width", "height" and "pxArray" of "bmpImgBuf" to an unpadded copy of the of the parameter "bmpFile".
 Pixelarray starts in bottom left of picture.
 */
 int parseBMPFile(const void* buf, size_t bufSize, uBMPImage* bmpImgBuf) {
     if (bufSize < 26) {
         fprintf(stderr, "Error: file too small\n");
-        return -1;
+        return 1;
     }
 
     if (*(uint16_t*) buf != BMP_HEADER_SIGN) {
         fprintf(stderr, "Error: incorrect file header signature\n");
-        return -1;
+        return 1;
     }
 
     if (*(uint32_t*)(buf + FILESIZE_OFFS) != bufSize) {
         fprintf(stderr, "Error: file size not matching size specified in file info\n");
-        return -1;
+        return 1;
     }
 
     int32_t pxWidth = *(int32_t*)(buf + PXWIDTH_OFFS);
     if (pxWidth < 0) {
         fprintf(stderr, "Error: image width must't be negative");
-        return -1;
+        return 1;
     }
 
     int negHeight = 0;
@@ -93,13 +93,13 @@ int parseBMPFile(const void* buf, size_t bufSize, uBMPImage* bmpImgBuf) {
 
     if (dataOffset + byteWidthPadded * pxHeight > bufSize) {
         fprintf(stderr, "Error: file size doesn't match file info\n");
-        return -1;
+        return 1;
     }
     
     pixel24_t* pxArray = malloc(byteWidth * pxHeight);
     if (!pxArray) {
         fprintf(stderr, "Error: failed allocating memory for pixel array\n");
-        return -1;
+        return 1;
     }
     
     pixel24_t* pxArrayEnd = pxArray + pxWidth * pxHeight;
---
+++ Implementierung/src/Implementierung/test_basic_sobel.c
@@ -0,0 +1,96 @@
+#include "basic_sobel.h"
+#include "../IOSystem/bmp_parser.h"
+#include "../Testsystem/unittest.h"
+
+#include "stdio.h"
+#include "stdlib.h"
+
+size_t buffer_size;
+void* raw_img_data;
+uBMPImage *bmpImage;
+
+int testColorOfPixelRed() {
+    int x = 1;
+    int y = 1;
+    return ASSERT_EQUAL_U_INT8(34, colorOfPixel(bmpImage->pxArray, bmpImage->pxWidth, x, y, RED));
+}
+
+int testColorOfPixelGreen() {
+    int x = 2;
+    int y = 2;
+    return ASSERT_EQUAL_U_INT8(133, colorOfPixel(bmpImage->pxArray, bmpImage->pxWidth, x, y, GREEN));
+}
+
+int testColorOfPixelBlue() {
+    int x = 2;
+    int y = 1;
+    return ASSERT_EQUAL_U_INT8(102, colorOfPixel(bmpImage->pxArray, bmpImage->pxWidth, x,y,BLUE));
+}
+
+int testSetColorOfPixel() {
+    uint8_t red = 32;
+    uint8_t green = 42;
+    uint8_t blue = 52;
+    int x = 6;
+    int y = 7;
+    setPixelAt(bmpImage->pxArray, bmpImage->pxWidth, 6, 7, red, green, blue);
+    int passed = 1;
+    passed &= ASSERT_EQUAL_U_INT8(red, colorOfPixel(bmpImage->pxArray, bmpImage->pxWidth, x, y, RED));
+    passed &= ASSERT_EQUAL_U_INT8(green, colorOfPixel(bmpImage->pxArray, bmpImage->pxWidth, x, y, GREEN));
+    passed &= ASSERT_EQUAL_U_INT8(blue, colorOfPixel(bmpImage->pxArray, bmpImage->pxWidth, x,y,BLUE));
+    return passed;
+}
+
+int testSobelSmall() {
+    sobel(bmpImage->pxArray, bmpImage->pxWidth, bmpImage->pxHeight, bmpImage->pxArray);
+}
+
+
+
+int setUp() {
+    char* path = "../res/muster.bmp";
+    fprintf(stdout, "Loading BMP File: %s for test data\n", path);
+    raw_img_data = readBMPFile(path, &buffer_size);
+
+    if (raw_img_data == NULL){
+        fprintf(stderr, "Couldn't read BMP File\n");
+        return 1;
+    }
+    bmpImage = malloc(sizeof(uBMPImage));
+    if (bmpImage == NULL){
+        fprintf(stderr, "Couldn't allocate memory for BMP Image\n");
+        return 1;
+    }
+    if (parseBMPFile(raw_img_data, buffer_size, bmpImage) == 1) {
+        fprintf(stderr, "Couldn't parse BMP file");
+        return 1;
+    }
+    return 0;
+}
+
+int tearDown() {
+    free(raw_img_data);
+    free(bmpImage);
+    return 0;
+}
+
+int runTests(void){
+    startTesting(__BASE_FILE__);
+    if (setUp() == 1){
+        tearDown();
+        return 1;
+    }
+    runTest(testColorOfPixelRed);
+    runTest(testColorOfPixelBlue);
+    runTest(testColorOfPixelGreen);
+    runTest(testSetColorOfPixel);
+    stopTesting();
+    tearDown();
+    return 0;
+}
+
+int main(void){
+    runTests();
+}
+
+
