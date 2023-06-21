a3a522fef67903b137eb433245d772c37e868e83
Simon Bußmann <simon.bussmann@tum.de>
2023-06-22 00:09:10 +0200

    Added imgage_similarity test and fixed warnings

--- Implementierung/src/Implementierung/test_basic_sobel.c
+++ Implementierung/src/Implementierung/test_basic_sobel.c
@@ -1,31 +1,52 @@
 #include "basic_sobel.h"
 #include "../IOSystem/bmp_parser.h"
-#include "../Testsystem/unittest.h"
 #include "../IOSystem/libbmp.h"
+#include "../Testsystem/unittest.h"
+#include "../Testsystem/image_similarity.h"
 
 #include "stdio.h"
 #include "stdlib.h"
 
-size_t buffer_size;
-void* raw_img_data;
+size_t img_size;
 uBMPImage *bmpImage;
+
+size_t loadPicture(char* path, uBMPImage* img) {
+    void* buffer;
+    size_t buffer_size;
+    fprintf(stdout, "Loading BMP File: %s for test data\n", path);
+    buffer = readBMPFile(path, &buffer_size);
+
+    if (buffer == NULL){
+        fprintf(stderr, "Couldn't read BMP File\n");
+        return 0;
+    }
+    if (img == NULL){
+        fprintf(stderr, "img was not initialized\n");
+        return 0;
+    }
+    if (parseBMPFile(buffer, buffer_size, img) == 1) {
+        fprintf(stderr, "Couldn't parse BMP file");
+        return 0;
+    }
+    return buffer_size;
+}
 
 int testColorOfPixelRed() {
     int x = 1;
     int y = 1;
-    return ASSERT_EQUAL_U_INT8(34, colorOfPixel(bmpImage->pxArray, bmpImage->pxWidth, x, y, RED));
+    return ASSERT_EQUAL_U_INT8(34, colorOfPixel((uint8_t *) bmpImage->pxArray, bmpImage->pxWidth, x, y, RED));
 }
 
 int testColorOfPixelGreen() {
     int x = 2;
     int y = 2;
-    return ASSERT_EQUAL_U_INT8(133, colorOfPixel(bmpImage->pxArray, bmpImage->pxWidth, x, y, GREEN));
+    return ASSERT_EQUAL_U_INT8(133, colorOfPixel((uint8_t *) bmpImage->pxArray, bmpImage->pxWidth, x, y, GREEN));
 }
 
 int testColorOfPixelBlue() {
     int x = 2;
     int y = 1;
-    return ASSERT_EQUAL_U_INT8(102, colorOfPixel(bmpImage->pxArray, bmpImage->pxWidth, x,y,BLUE));
+    return ASSERT_EQUAL_U_INT8(102, colorOfPixel((uint8_t *) bmpImage->pxArray, bmpImage->pxWidth, x,y,BLUE));
 }
 
 int testSetColorOfPixel() {
@@ -34,17 +55,17 @@ int testSetColorOfPixel() {
     uint8_t blue = 52;
     int x = 6;
     int y = 7;
-    setPixelAt(bmpImage->pxArray, bmpImage->pxWidth, 6, 7, red, green, blue);
+    setPixelAt((uint8_t *) bmpImage->pxArray, bmpImage->pxWidth, 6, 7, red, green, blue);
     int passed = 1;
-    passed &= ASSERT_EQUAL_U_INT8(red, colorOfPixel(bmpImage->pxArray, bmpImage->pxWidth, x, y, RED));
-    passed &= ASSERT_EQUAL_U_INT8(green, colorOfPixel(bmpImage->pxArray, bmpImage->pxWidth, x, y, GREEN));
-    passed &= ASSERT_EQUAL_U_INT8(blue, colorOfPixel(bmpImage->pxArray, bmpImage->pxWidth, x,y,BLUE));
+    passed &= ASSERT_EQUAL_U_INT8(red, colorOfPixel((uint8_t *) bmpImage->pxArray, bmpImage->pxWidth, x, y, RED));
+    passed &= ASSERT_EQUAL_U_INT8(green, colorOfPixel((uint8_t *) bmpImage->pxArray, bmpImage->pxWidth, x, y, GREEN));
+    passed &= ASSERT_EQUAL_U_INT8(blue, colorOfPixel((uint8_t *) bmpImage->pxArray, bmpImage->pxWidth, x,y,BLUE));
     return passed;
 }
 
-int testSobelSmall() {
+int generateBMPofSobel() {
     pixel24_t* newPixels = malloc(sizeof(bmpImage->pxArray));
-    sobel(bmpImage->pxArray, bmpImage->pxWidth, bmpImage->pxHeight, newPixels);
+    sobel((uint8_t *) bmpImage->pxArray, bmpImage->pxWidth, bmpImage->pxHeight, (uint8_t *) newPixels);
     bmp_img img;
     bmp_img_init_df(&img, bmpImage->pxWidth, bmpImage->pxHeight);
     for (size_t y = 0; y < bmpImage->pxWidth; y++)
@@ -60,31 +81,46 @@ int testSobelSmall() {
     return 0;
 }
 
+int testSobel() {
+    pixel24_t* newPixels = malloc(sizeof(bmpImage->pxArray));
+    sobel((uint8_t *) bmpImage->pxArray, bmpImage->pxWidth, bmpImage->pxHeight, (uint8_t *) newPixels);
 
+    char* reference_path = "../res/johnmuirtrail_correct_opencv.bmp";
+
+    size_t reference_buffer_size;
+    uBMPImage *reference_bmpImage = malloc(sizeof(uBMPImage));
+
+    reference_buffer_size = loadPicture(reference_path, reference_bmpImage);
+
+    if (reference_buffer_size == 0) {
+        fprintf(stderr, "Couldn't load picture\n");
+        free(reference_bmpImage);
+    }
+
+    double similarity = compareImages((uint8_t*) bmpImage->pxArray,
+                                      bmpImage->pxWidth * bmpImage->pxHeight * 3,
+                                      (uint8_t*) reference_bmpImage->pxArray,
+                                      reference_bmpImage->pxWidth * reference_bmpImage->pxHeight * 3);
+    printf("similarity: %3.3f %%\n", (similarity*100));
+    double similarity_without_hue = compareImages((uint8_t*) bmpImage->pxArray,
+                                      bmpImage->pxWidth * bmpImage->pxHeight * 3,
+                                                  (uint8_t*)  reference_bmpImage->pxArray,
+                                      reference_bmpImage->pxWidth * reference_bmpImage->pxHeight * 3);
+    printf("similarity_without_hue: %3.3f %%\n", (similarity_without_hue*100));
+    return 0;
+}
 
 int setUp() {
     char* path = "../res/johnmuirtrail.bmp";
-    fprintf(stdout, "Loading BMP File: %s for test data\n", path);
-    raw_img_data = readBMPFile(path, &buffer_size);
-
-    if (raw_img_data == NULL){
-        fprintf(stderr, "Couldn't read BMP File\n");
-        return 1;
-    }
     bmpImage = malloc(sizeof(uBMPImage));
-    if (bmpImage == NULL){
-        fprintf(stderr, "Couldn't allocate memory for BMP Image\n");
-        return 1;
-    }
-    if (parseBMPFile(raw_img_data, buffer_size, bmpImage) == 1) {
-        fprintf(stderr, "Couldn't parse BMP file");
+    img_size = loadPicture(path, bmpImage);
+    if (img_size == 0){
         return 1;
     }
     return 0;
 }
 
 int tearDown() {
-    free(raw_img_data);
     free(bmpImage);
     return 0;
 }
@@ -99,7 +135,8 @@ int runTests(void){
     runTest(testColorOfPixelBlue);
     runTest(testColorOfPixelGreen);
     runTest(testSetColorOfPixel);
-    testSobelSmall();
+    generateBMPofSobel();
+    testSobel();
     stopTesting();
     tearDown();
     return 0;
