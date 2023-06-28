af1f0ac92ef8175eac00e50e4362ca4e0b5f16aa
Nico <nico.lintner@tum.de>
2023-06-28 13:57:47 +0200

    creating header/infoHeader in seperate function

--- Implementierung/src/IOSystem/bmp_parser.c
+++ Implementierung/src/IOSystem/bmp_parser.c
@@ -33,7 +33,7 @@ int bmpToArray(char* buf, size_t bufSize, uBMPImage* bmpImgBuf) {
     }
 
     if (*(uint32_t*)(buf + FILESIZE_OFFS) != bufSize) {
-        fprintf(stderr, "Error: file size not matching size specified in file info\n");
+        fprintf(stderr, "Error: file size not matching size specified in file info\n"); 
         return 1;
     }
 
@@ -90,6 +90,27 @@ int bmpToArray(char* buf, size_t bufSize, uBMPImage* bmpImgBuf) {
     return 0;
 }
 
+
+//const char headerInfoHeaderTemplate[HEADER_SIZE + INFO_HEADER_SIZE] = {0x42, 0x4D, 0, 0, 0, 0, 0, 0, 0, 0, INFO_HEADER_SIZE + HEADER_SIZE, 0x0, 0x0, 0x0, INFO_HEADER_SIZE, 0x0, 0x0, 0x0, 0, 0, 0, 0, 0, 0, 0, 0, 0x1, 0x0, }
+#define NUM_PLANES 1
+#define PX_PER_METER 3800
+#define BIT_DEPTH 3 * 8
+#define NUM_COLORS 256 * 256 * 256
+
+static inline void generateHeaderInfoHeader(char* zeroBuf, uint32_t fileSize, int32_t width, int32_t height) {
+    *(uint16_t*)(zeroBuf + 0x00) = BMP_HEADER_SIGN;
+    *(uint32_t*)(zeroBuf + 0x02) = fileSize;
+    *(uint32_t*)(zeroBuf + 0x0A) = HEADER_SIZE + INFO_HEADER_SIZE;
+    *(uint32_t*)(zeroBuf + 0x0E) = INFO_HEADER_SIZE;
+    *(uint32_t*)(zeroBuf + 0x12) = width;
+    *(uint32_t*)(zeroBuf + 0x16) = height;
+    *(uint16_t*)(zeroBuf + 0x1A) = NUM_PLANES;
+    *(uint16_t*)(zeroBuf + 0x1C) = BIT_DEPTH;
+    *(uint32_t*)(zeroBuf + 0x26) = PX_PER_METER;
+    *(uint32_t*)(zeroBuf + 0x2A) = PX_PER_METER;
+    *(uint32_t*)(zeroBuf + 0x2E) = NUM_COLORS;
+}
+
 /*
 Return a pointer to a buffer containing a complete BMP image.
 Writes size of buffer to parameter "size".
@@ -101,17 +122,7 @@ char* arrayToBmp(const uBMPImage* bmpImg, size_t* size) {
     *size = HEADER_SIZE + INFO_HEADER_SIZE + byteWidthPadded * (bmpImg->pxHeight - 2);
     char* buf = calloc(*size, 1);
 
-    *(uint16_t*)(buf + 0x0) = BMP_HEADER_SIGN;
-    *(uint32_t*)(buf + 0x2) = *size;
-    *(uint32_t*)(buf + 0xA) = HEADER_SIZE + INFO_HEADER_SIZE;
-    *(uint32_t*)(buf + 0xE) = INFO_HEADER_SIZE;
-    *(uint32_t*)(buf + 0x12) = bmpImg->pxWidth - 2;
-    *(uint32_t*)(buf + 0x16) = bmpImg->pxHeight - 2;
-    *(uint16_t*)(buf + 0x1A) = 1;
-    *(uint16_t*)(buf + 0x1C) = 24;
-    *(uint32_t*)(buf + 0x26) = 3800;
-    *(uint32_t*)(buf + 0x2A) = 3800;
-    *(uint32_t*)(buf + 0x2E) = 256 * 256 * 256;
+    generateHeaderInfoHeader(buf, *size, bmpImg->pxWidth - 2, bmpImg->pxHeight - 2);
 
     char* pxData = buf + HEADER_SIZE + INFO_HEADER_SIZE;
     pixel24_t* pxArraySrc = bmpImg->pxArray + bmpImg->pxWidth;
