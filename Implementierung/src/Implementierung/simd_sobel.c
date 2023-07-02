cd463304f3de509808d935b8845490f6e3730a6e
Manuel Walter Mußbacher <ge48gax@mytum.de>
2023-07-02 17:37:12 +0200

    SIMD = REF: CHECK
    (Bis auf schwarzen Rand)

--- Implementierung/src/Implementierung/simd_sobel.c
+++ Implementierung/src/Implementierung/simd_sobel.c
@@ -8,8 +8,6 @@
 #define SHIFT_BY2 2
 #define SHIFT_BY1 1
 
-const uint8_t cleanUp16BitRShifts[0];
-
 const uint8_t SHIFT_BY2_MASK[] =
         {
             0x3F, 0x3F, 0x3F, 0x3F, 0x3F,
@@ -31,27 +29,14 @@ const uint8_t ZERO_EVEN_BYTES_MASK[] =
             0xFF, 0x0,0xFF, 0x0, 0xFF, 0x0
         };
 
-//const uint8_t ZERO_UNEVEN_BYTES_MASK[] =
-//        {
-//                0x0, 0xFF, 0x0,0xFF, 0x0,
-//                0xFF, 0x0,0xFF, 0x0,0xFF,
-//                0x0,0xFF, 0x0, 0xFF, 0x0, 0xFF
-//        };
-
 const uint16_t COMP_255[] =
         {
         0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF
         };
 
 void simd_sobel(uint8_t* img_in, size_t width, size_t height, uint8_t* img_out) {
-    //grosszugig
     if (width >= 16) {
-        __m128i shrpi8Mask2 = _mm_loadu_si128((const __m128i*) SHIFT_BY2_MASK);
-        __m128i shrpi8Mask1 = _mm_loadu_si128((const __m128i*) SHIFT_BY1_MASK);
-
         __m128i zeroEvenBytesMask = _mm_loadu_si128((const __m128i*) ZERO_EVEN_BYTES_MASK);
-        //__m128i zeroUnevenBytesMask = _mm_loadu_si128((const __m128i*) ZERO_UNEVEN_BYTES_MASK);
-
         for (size_t i = width * 3 + 3; i < width * (height-1) * 3 - 3; i += 16) {
             __m128i A_v = _mm_setzero_si128();
             __m128i A_h = _mm_setzero_si128();
@@ -78,9 +63,12 @@ void simd_sobel(uint8_t* img_in, size_t width, size_t height, uint8_t* img_out) 
             __m128i lowerRight2 = _mm_loadu_si128((const __m128i*) (img_in + i + width * 3 + 3 + 1));
 
 
-            // Shifting 16 bit ints right by 2 and zeroing the two highest-order bits to simulate an 8 bit int shift.
-            // We need to do this in order to prevent overflows that would otherwise be practically impossible to detect.
-            // The downside of this is that the resulting image will be darker.
+            /*
+             * We avoid Byte overflows by using word sized integers. To do this, we load 16 Byte from the current
+             * address as well as from the current address + 1. This way we can use a bit mask to zero out every second byte
+             * to convert 16 bytes in one xmm register to 8 words in two xmm registers.
+            */
+
             upperLeft = _mm_and_si128(upperLeft,  zeroEvenBytesMask);
             upper = _mm_and_si128(upper,  zeroEvenBytesMask);
             upperRight = _mm_and_si128(upperRight,  zeroEvenBytesMask);
@@ -133,8 +121,8 @@ void simd_sobel(uint8_t* img_in, size_t width, size_t height, uint8_t* img_out) 
             A_h = _mm_sub_epi16(A_h, right);
             A_h = _mm_sub_epi16(A_h, right);
 
-            A_h = _mm_add_epi16(A_h, lowerRight);
-            A_h = _mm_sub_epi16(A_h, lowerLeft);
+            A_h = _mm_add_epi16(A_h, lowerLeft);
+            A_h = _mm_sub_epi16(A_h, lowerRight);
 
 
             A_h2 = _mm_add_epi16(A_h2, upperLeft2);
@@ -145,8 +133,8 @@ void simd_sobel(uint8_t* img_in, size_t width, size_t height, uint8_t* img_out) 
             A_h2 = _mm_sub_epi16(A_h2, right2);
             A_h2 = _mm_sub_epi16(A_h2, right2);
 
-            A_h2 = _mm_add_epi16(A_h2, lowerRight2);
-            A_h2 = _mm_sub_epi16(A_h2, lowerLeft2);
+            A_h2 = _mm_add_epi16(A_h2, lowerLeft2);
+            A_h2 = _mm_sub_epi16(A_h2, lowerRight2);
 
 
             A_v2 = _mm_abs_epi16(A_v2);
@@ -154,13 +142,17 @@ void simd_sobel(uint8_t* img_in, size_t width, size_t height, uint8_t* img_out) 
             A_v =  _mm_abs_epi16(A_v);
             A_h =  _mm_abs_epi16(A_h);
 
-            __m128i result = _mm_and_si128(_mm_cmpgt_epi16(_mm_add_epi16(A_v2, A_h2), _mm_loadu_si128((const __m128i*) COMP_255)), zeroEvenBytesMask);
-            __m128i result2 = _mm_and_si128(_mm_cmpgt_epi16(_mm_add_epi16(A_v, A_h), _mm_loadu_si128((const __m128i*) COMP_255)), zeroEvenBytesMask);
+            __m128i sum = _mm_add_epi16(A_v, A_h);
+            __m128i sum2 =_mm_add_epi16(A_v2, A_h2);
+            
+            __m128i result = _mm_and_si128(_mm_cmpgt_epi16(sum, _mm_loadu_si128((const __m128i*) COMP_255)), zeroEvenBytesMask);
+            __m128i result2 = _mm_and_si128(_mm_cmpgt_epi16(sum2, _mm_loadu_si128((const __m128i*) COMP_255)), zeroEvenBytesMask);
 
-            result = _mm_or_si128(result, _mm_add_epi16(A_v, A_h));
-            result2 = _mm_or_si128(result2, _mm_add_epi16(A_v2, A_h2));
+            result = _mm_or_si128(result, sum);
+            result2 = _mm_or_si128(result2, sum2);
+
             result2 = _mm_slli_epi16(result2, 8);
-            // While it is still possible for this last sum to overflow, the chances are fairly low.
+
             _mm_storeu_si128((__m128i*) (img_out + i), _mm_or_si128(result2, result));
         }
     } else {
