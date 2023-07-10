f20f4bc05083840eacbf4a810dcff5baed7d240e
Manuel Walter Mußbacher <ge48gax@mytum.de>
2023-07-11 01:57:01 +0200

    Extractet duplicate SIMD code block into own function

--- Implementierung/src/Implementierung/simd_sobel.h
+++ Implementierung/src/Implementierung/simd_sobel.h
@@ -1,6 +1,8 @@
 #include <stdint.h>
 #include <stddef.h>
+#include <immintrin.h>
 
 extern const uint16_t COMP_255[];
 extern const uint8_t ZERO_EVEN_BYTES_MASK[];
 void simd_sobel(uint8_t* img_in, size_t width, size_t height, uint8_t* img_out);
+void computeSIMDSobel(uint8_t* img_in, size_t i, size_t width, uint8_t* img_out, __m128i zeroEvenBytesMask, __m128i comparer);
