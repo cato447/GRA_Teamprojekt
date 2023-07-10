f748a89d571fdaf1afbaa5302934024992834411
Manuel Walter Mußbacher <ge48gax@mytum.de>
2023-07-11 01:59:49 +0200

    Added attribute to header

--- Implementierung/src/Implementierung/simd_sobel.h
+++ Implementierung/src/Implementierung/simd_sobel.h
@@ -5,4 +5,6 @@
 extern const uint16_t COMP_255[];
 extern const uint8_t ZERO_EVEN_BYTES_MASK[];
 void simd_sobel(uint8_t* img_in, size_t width, size_t height, uint8_t* img_out);
+
+__attribute__((__always_inline__, __hot__)) inline
 void computeSIMDSobel(uint8_t* img_in, size_t i, size_t width, uint8_t* img_out, __m128i zeroEvenBytesMask, __m128i comparer);
