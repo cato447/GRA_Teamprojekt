d1e635abdc86174889adf07b1dd0930dbac89c7a
Simon Bußmann <simon.bussmann@tum.de>
2023-07-15 16:17:24 +0200

    added tools for plotting

---
+++ Implementierung/performance/create_plots.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import sys
+import matplotlib.pyplot as plt
+
+def load_df_from_csv(csv_file: str):
+    df = pd.read_csv(csv_file, sep=",")
+    df = df.rename(columns={'avg_exec_time' : f'version_{df.get("version")[0]}'})
+    return df
+
+if __name__ == '__main__':
+    dataframes = []
+    for path in sys.argv[1:]:
+        dataframes.append(load_df_from_csv(path))
+    if len(sys.argv) == 2:
+        df = dataframes[0]
+        res = df.plot(x='pixelNum', y=[f'version_{df.get("version")[0]}'], style='.-', title="Performance Test", xlabel="Pixel", ylabel="Execution time (s)").get_figure()
+        res.savefig("performance.png")
+    else:
+        df = dataframes[0]
+        ax = df.plot(x='pixelNum', y=[f'version_{df.get("version")[0]}'], style='.-', title="Performance Test", xlabel="Pixel", ylabel="Execution time (s)") 
+        for df in dataframes[1:]:
+            df.plot(ax=ax, x='pixelNum', y=[f'version_{df.get("version")[0]}'], style='.-')
+        
+        ax.get_figure().savefig("performance.png")
