b3778d3dbef35637395c184295fe0ef6a6098f9c
Nico <nico.lintner@tum.de>
2023-07-16 18:17:56 +0200

    added accidentally removed files again

---
+++ Implementierung/src/IOSystem/load_save_utils.h
@@ -0,0 +1,10 @@
+#ifndef _BMP_LOADER_H_
+#define _BMP_LOADER_H_
+
+#include "s_image.h"
+
+s_image *load_image(const char *path, int in_graysc);
+int save_image(s_image *img_buf, int in_graysc, const char *path);
+void free_image(s_image *img);
+
+#endif
